3ed9fbaf9115958bfa61b1c945d10e1666f88e93
Felix Bade <flix.bade@gmail.com>
2020-10-11 23:21:41 +0300

    Add support for multiple chats

--- lib/app.ts
+++ lib/app.ts
@@ -14,22 +14,23 @@ app.get('/', async (req, res) => {
     res.send('See https://github.com/felixbade/github-telegram-notifications')
 })
 
-app.post('/', async (req, res) => {
+app.post('/:chat_id(-?\\d+)', async (req, res) => {
     const body = req.body
+    const chat_id = parseInt(req.params.chat_id)
 
     console.log(JSON.stringify(body, null, 4))
 
     if (body.commits !== undefined) {
         const messageToTelegram = formatCommits(body)
         if (messageToTelegram !== undefined) {
-            await sendMessage(messageToTelegram)
+            await sendMessage(chat_id, messageToTelegram)
         }
     }
 
     if (body.pull_request !== undefined) {
         const messageToTelegram = formatPullRequest(body)
         if (messageToTelegram !== undefined) {
-            await sendMessage(messageToTelegram)
+            await sendMessage(chat_id, messageToTelegram)
         }
     }
 
--- lib/telegram.ts
+++ lib/telegram.ts
@@ -1,10 +1,10 @@
-import { default as axios, AxiosResponse } from 'axios'
+import { default as axios } from 'axios'
 
-export const sendMessage = async (text: string) => {
+export const sendMessage = async (chat_id: number, text: string) => {
     console.log('Sending to Telegram:', text)
     await axios.post(`https://api.telegram.org/bot${process.env.TELEGRAM_SECRET}/sendMessage`, {
         // eslint-disable-next-line @typescript-eslint/camelcase
-        chat_id: process.env.TELEGRAM_CHAT,
+        chat_id,
         text,
         disable_web_page_preview: true,
         parse_mode: 'HTML'
